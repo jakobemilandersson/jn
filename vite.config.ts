244799b49aa45b396bb57da53c8e1162ae56088f
Jakob Andersson <jakobemilandersson@gmail.com>
2025-12-06 16:44:08 +0100

    fix: GH Pages deploy action

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/' // For GitHub Pages, base may change if repo is a project page
+  base: '/  ' // For GitHub Pages, base may change if repo is a project page
 })
