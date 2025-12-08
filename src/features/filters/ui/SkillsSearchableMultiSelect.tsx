5192b71859933e27586d96cadee550449e357e93
Jakob Andersson <jakobemilandersson@gmail.com>
2025-12-08 08:49:31 +0100

    add: searchable multiselect filter component

---
+++ src/features/filters/ui/SkillsSearchableMultiSelect.tsx
@@ -0,0 +1,17 @@
+import { SKILL_OPTIONS } from "../lib/getSkillOptions";
+import { useFilterStore } from "../model/useFilterStore";
+import { SearchableMultiSelect } from "../../../shared/ui/SearchableMultiSelect";
+
+export function SkillsSearchableMultiSelect() {
+  const skills = useFilterStore(s => s.skills);
+  const setSkills = useFilterStore(s => s.setSkills);
+
+  return (
+    <SearchableMultiSelect
+      label="Skills"
+      options={SKILL_OPTIONS}
+      selected={skills}
+      onChange={setSkills}
+    />
+  );
+}
