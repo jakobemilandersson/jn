64f7853584cb9ac6eaf946198951f7e00d2b9fc3
Jakob Andersson <jakobemilandersson@gmail.com>
2025-12-16 13:16:50 +0100

    fix: simplify duplication filtering

--- src/entities/resume/lib/skillIndex.ts
+++ src/entities/resume/lib/skillIndex.ts
@@ -2,22 +2,9 @@ import type { WorkExperience, Skill } from "../types";
 import { RESUME } from "../data";
 
 export const extractSkills = (data: WorkExperience[]): Skill[] => {
-  const duplicateSkills = data.flatMap((r) => r.skills);//.map((s) => [s.presentation, s])
-  let uniqueSkills: Skill[] = []
-  duplicateSkills.forEach((s) => {
-    const skillPresentation = s.presentation
-    const uniqueSkillPresentations = uniqueSkills.map((s) => s.presentation)
-    if(!(uniqueSkillPresentations.includes(skillPresentation))) {
-      uniqueSkills.push(s);
-    }
-  });
-
-  return uniqueSkills;
-  return Array.from(
-    new Map(
-      data.flatMap((r) => r.skills).map((s) => [s.presentation, s])
-    ).values()
-  );
+  return data.flatMap((r) => r.skills).filter((s, i, arr) =>
+    arr.findIndex(s2 => (s2.presentation === s.presentation)) === i
+  )
 }
 
 export const getAllSkills = (): Skill[] => extractSkills(RESUME);
