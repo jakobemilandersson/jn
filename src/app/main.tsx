42df2b26ca62d7c3ad7b691894ed4a70a07df51c
Jakob Andersson <jakobemilandersson@gmail.com>
2026-01-04 19:32:03 +0100

    chore(app): mount decorative space background at root

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Footer } from '@shared/ui'
 import ResumePage from '@pages/ResumePage'
+import { SpaceBackground } from "./SpaceBackground";
 import './styles.css'
 
 function App() {
   return <React.StrictMode>
     <main className="min-h-screen px-4">
+      <SpaceBackground />
       <ResumePage />
       <Footer />
     </main>
